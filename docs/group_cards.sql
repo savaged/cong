c6b8678a7d6327eb5e0e0342ffdcd8242b5a18a3
savaged <david@savaged.info>
2009-10-27 21:31:35 +0000

    simple script to re-use for fetching group cards from db

--- docs/group_cards.sql
+++ docs/group_cards.sql
@@ -1,12 +1,14 @@
 select 
 m.lastname + ', ' + m.firstname as fullname,
+r.yyyymm,
 r.books,
 r.brochures,
 r.hours,
 r.magazines,
 r.return_visits,
-r.studies
+r.studies,
+r.comments
 from service_report r
 join member m on r.publisher_id = m.id
 where m.group_unit = 'VULCAN_WAY'
-
+order by fullname, r.yyyymm
